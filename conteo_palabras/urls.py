c503739e7f252d9822ef63ce9382220fa48ef583
Cristian Martinez <cristianmartinez@Cristians-MacBook-Pro.local>
2020-02-03 22:08:36 -0500

    Resuelvo reto 5

--- conteo_palabras/urls.py
+++ conteo_palabras/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from conteo_palabras.views import ContadorView, contadorPalabras, cantPalabrasArchivo, topNPalabras, topNPalabrasR4
+from conteo_palabras.views import ContadorView, contadorPalabras, cantPalabrasArchivo, topNPalabras, topNPalabrasR4, topNPalabrasR5
 
 urlpatterns = [
     path('reto_1/', contadorPalabras, name='cuenta_palabras'),
     path('reto_2/', cantPalabrasArchivo, name='cant_palabras'),
     path('reto_3/', topNPalabras, name='top_n_palabras'),
     path('reto_4/', topNPalabrasR4, name='top_n_palabras'),
+    path('reto_5/', topNPalabrasR5, name='top_n_palabras'),
     
 ]
