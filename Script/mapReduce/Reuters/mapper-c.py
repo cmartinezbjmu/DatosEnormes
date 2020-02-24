736ac45966dec957a4552ddb1f4f442367176892
Cristian Martinez <cristianmartinez@Cristians-MacBook-Pro.local>
2020-02-23 23:51:10 -0500

    Soluciono punto 3 del taller

--- Script/mapReduce/Reuters/mapper-c.py
+++ Script/mapReduce/Reuters/mapper-c.py
@@ -5,10 +5,26 @@ import re
 
 patron_body = re.compile('<BODY>')
 patron_body_close = re.compile('</BODY>')
+patron_titulo = re.compile('<TITLE>')
+patron_fecha = re.compile('<DATE>')
 total_noticias = 0
+longitud_palabra = 0
+titulo = ""
 
 flag = False
 for linea in sys.stdin:
+    if patron_fecha.match(linea):
+        linea = linea.split('<DATE>')[1]
+        linea = linea.split('</DATE>')[0]
+        linea.strip()
+        fecha = re.sub("<.*?>", "", linea).lower()
+
+    if patron_titulo.match(linea):
+        linea = linea.split('<TITLE>')[1]
+        linea = linea.split('</TITLE>')[0]
+        linea.strip()
+        titulo = re.sub("<.*?>", "", linea).lower()
+
     if patron_body.findall(linea):
         total_palabras = 0
         linea = linea.split('<BODY>')[1]
@@ -21,13 +37,17 @@ for linea in sys.stdin:
         body = body.strip()        
         palabras = body.split()
         total_palabras += len(palabras)
-    
-    if patron_body_close.findall(linea):        
-        total_noticias += 1
-        print '%s\t%s' % (total_noticias, total_palabras)
-        #print("{}\t{}".format(total_noticias, total_palabras))
-           
-            
+        for palabra in palabras:
+            if longitud_palabra < len(palabra):
+                longitud_palabra = len(palabra)
+                palabra_larga = palabra
+        titulo_palabra = titulo
+        fecha_palabra = fecha
         
-    
+    if patron_body_close.findall(linea): 
+        flag = False       
+        total_noticias += 1
+        longitud_palabra = 0
+        #print '%s\t%s' % (total_noticias, total_palabras)
+        print("{}\t{}\t{}\t{}".format(total_palabras, palabra_larga, titulo_palabra, fecha_palabra))
     
--- Script/mapReduce/Reuters/reducer-c.py
+++ Script/mapReduce/Reuters/reducer-c.py
@@ -8,20 +8,35 @@ import sys
 
 total_noticias = 0
 total_palabras = 0
+longitud = 0
+palabra = ""
+max_palabras = 0
 
 # La entrada proviene de la salida del mapper
 for linea in sys.stdin:
     # Elimina espacios en blanco
     linea = linea.strip()
-    # asociasion de palabras y conteo
-    n, conteo = linea.split('\t', 1)   
-
+    # asociacion de palabras y conteo
+    conteo, palabra_larga, titulo, fecha = linea.split('\t')
+    
     try:
-        conteo = int(conteo)        
+        conteo = int(conteo)
+
     except ValueError:
         continue
     total_noticias += 1    
-    total_palabras += conteo    
+    total_palabras += conteo
+    if len(palabra) < len(palabra_larga):
+        palabra = palabra_larga
+        titulo_larga = titulo
+        fecha_larga = fecha
+    
+    if max_palabras < conteo:
+        max_palabras = conteo
+        titulo_max_palabras = titulo
+    
 
-print 'Promedio de palabras en las noticias es: {:0.2f}'.format((total_palabras/float(total_noticias)))
-#print('Promedio de palabras en las noticias es: {}'.format(total_palabras/float(total_noticias)))
+#print 'Promedio de palabras en las noticias es: {:0.2f}'.format((total_palabras/float(total_noticias)))
+print('Promedio de palabras en las noticias es: {}'.format(total_palabras/total_noticias))
+print('La palabra mas larga es: {} y el titulo de la noticia es: {} publicada en la fecha: {}'.format(palabra, titulo_larga, fecha_larga))
+print('La noticia que tiene mas palabras es: {} con {} palabras'.format(titulo_max_palabras, conteo))
