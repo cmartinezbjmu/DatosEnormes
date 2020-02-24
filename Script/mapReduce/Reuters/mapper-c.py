001bc8be52fb07f59619c9d7707b5860f1619dc7
Cristian Martinez <cristianmartinez@Cristians-MacBook-Pro.local>
2020-02-24 00:03:36 -0500

    Complemento punto 3

--- Script/mapReduce/Reuters/mapper-c.py
+++ Script/mapReduce/Reuters/mapper-c.py
@@ -7,12 +7,20 @@ patron_body = re.compile('<BODY>')
 patron_body_close = re.compile('</BODY>')
 patron_titulo = re.compile('<TITLE>')
 patron_fecha = re.compile('<DATE>')
+patron_pais = re.compile('<PLACES>')
 total_noticias = 0
 longitud_palabra = 0
 titulo = ""
 
 flag = False
 for linea in sys.stdin:
+    if patron_pais.match(linea):
+        linea = linea.split('<PLACES>')[1]
+        linea = linea.split('</PLACES>')[0]
+        linea.strip()
+        linea = linea.replace('</D>', ' ')
+        pais = re.sub("<.*?>", "", linea).lower()
+
     if patron_fecha.match(linea):
         linea = linea.split('<DATE>')[1]
         linea = linea.split('</DATE>')[0]
@@ -43,11 +51,12 @@ for linea in sys.stdin:
                 palabra_larga = palabra
         titulo_palabra = titulo
         fecha_palabra = fecha
+        pais_noticia_max = pais
         
     if patron_body_close.findall(linea): 
         flag = False       
         total_noticias += 1
         longitud_palabra = 0
         #print '%s\t%s' % (total_noticias, total_palabras)
-        print("{}\t{}\t{}\t{}".format(total_palabras, palabra_larga, titulo_palabra, fecha_palabra))
+        print("{}\t{}\t{}\t{}\t{}".format(total_palabras, palabra_larga, titulo_palabra, fecha_palabra, pais_noticia_max))
     
--- Script/mapReduce/Reuters/reducer-c.py
+++ Script/mapReduce/Reuters/reducer-c.py
@@ -17,7 +17,7 @@ for linea in sys.stdin:
     # Elimina espacios en blanco
     linea = linea.strip()
     # asociacion de palabras y conteo
-    conteo, palabra_larga, titulo, fecha = linea.split('\t')
+    conteo, palabra_larga, titulo, fecha, pais = linea.split('\t')
     
     try:
         conteo = int(conteo)
@@ -34,9 +34,12 @@ for linea in sys.stdin:
     if max_palabras < conteo:
         max_palabras = conteo
         titulo_max_palabras = titulo
+        pais_noticia_max = pais
     
 
 #print 'Promedio de palabras en las noticias es: {:0.2f}'.format((total_palabras/float(total_noticias)))
+#print 'Promedio de palabras en las noticias es: {:0.2f}'.format((total_palabras/float(total_noticias)))
+#print 'Promedio de palabras en las noticias es: {:0.2f}'.format((total_palabras/float(total_noticias)))
 print('Promedio de palabras en las noticias es: {}'.format(total_palabras/total_noticias))
 print('La palabra mas larga es: {} y el titulo de la noticia es: {} publicada en la fecha: {}'.format(palabra, titulo_larga, fecha_larga))
-print('La noticia que tiene mas palabras es: {} con {} palabras'.format(titulo_max_palabras, conteo))
+print('La noticia que tiene mas palabras es: {} con {} palabras, publicada en {}'.format(titulo_max_palabras, conteo, pais))
