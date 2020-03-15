650c547f28547ae372dff689890135b6ded02dd8
jcardenascAndes93 <j.cardenasc@uniandes.edu.co>
2020-03-14 20:14:32 -0500

    Fix update

--- Script/mapReduce/Reuters/taxis/mapper_1.py
+++ Script/mapReduce/Reuters/taxis/mapper_1.py
@@ -14,9 +14,9 @@ for linea in sys.stdin:
         l = linea.split(',')
     except IndexError as e:
         pass
-    
-    if l:        
-        if 'vendorid' not in l[0].lower():            
+
+    if l:
+        if 'vendorid' not in l[0].lower():
             if len(l) == 18:
                 tipo_vehi = 'yellow'
                 destino = l[8]
@@ -29,16 +29,20 @@ for linea in sys.stdin:
             if len(l) == 7:
                 tipo_vehi = 'hfhv'
                 destino = l[5]
-            # capturar hora
-            fecha_hora = l[1].split()
-            fecha = fecha_hora[0].split('-')
-            hora = fecha_hora[1].split(':')[0]
-            hora = int(hora)
 
-            # comparar hora dataset con franja horaria
-            if (franja_hor[0] <= hora) and (hora <= franja_hor[1]):                
-                dia_semana = datetime.datetime(
-                    int(fecha[0]), int(fecha[1]), int(fecha[2])).weekday()
-                # entrega salida al reducer
-                #print('{},{},{}'.format(tipo_vehi, dia_semana, destino))
-                print '%s,%s,%s' % (tipo_vehi, dia_semana, destino)
+            try:
+                # capturar hora
+                fecha_hora = l[1].split()
+                fecha = fecha_hora[0].split('-')
+                hora = fecha_hora[1].split(':')[0]
+                hora = int(hora)
+                # comparar hora dataset con franja horaria
+                if (franja_hor[0] <= hora) and (hora <= franja_hor[1]):
+                    dia_semana = datetime.datetime(
+                        int(fecha[0]), int(fecha[1]), int(fecha[2])).weekday()
+                    # entrega salida al reducer
+                    #print('{},{},{}'.format(tipo_vehi, dia_semana, destino))
+                    print '%s,%s,%s' % (tipo_vehi, dia_semana, destino)
+
+            except IndexError as e:
+                pass
