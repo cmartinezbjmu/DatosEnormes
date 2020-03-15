7906e8b898b11bc11a06f94f990dccd89df1d759
jcardenascAndes93 <j.cardenasc@uniandes.edu.co>
2020-03-14 20:12:32 -0500

    Segementa la columna de destino segun el archivo

--- Script/mapReduce/Reuters/taxis/mapper_1.py
+++ Script/mapReduce/Reuters/taxis/mapper_1.py
@@ -7,6 +7,7 @@ import datetime
 franja_hor = [0, 1]
 tipo_vehi = ''
 l = None
+destino = None
 for linea in sys.stdin:
     linea.strip()
     try:
@@ -18,12 +19,16 @@ for linea in sys.stdin:
         if 'vendorid' not in l[0].lower():            
             if len(l) == 18:
                 tipo_vehi = 'yellow'
+                destino = l[8]
             if len(l) == 19:
                 tipo_vehi = 'green'
+                destino = l[6]
             if len(l) == 6:
                 tipo_vehi = 'fhv'
+                destino = l[4]
             if len(l) == 7:
                 tipo_vehi = 'hfhv'
+                destino = l[5]
             # capturar hora
             fecha_hora = l[1].split()
             fecha = fecha_hora[0].split('-')
@@ -31,11 +36,9 @@ for linea in sys.stdin:
             hora = int(hora)
 
             # comparar hora dataset con franja horaria
-            if (franja_hor[0] <= hora) and (hora <= franja_hor[1]):
-                destino = l[8]
+            if (franja_hor[0] <= hora) and (hora <= franja_hor[1]):                
                 dia_semana = datetime.datetime(
                     int(fecha[0]), int(fecha[1]), int(fecha[2])).weekday()
-
                 # entrega salida al reducer
                 #print('{},{},{}'.format(tipo_vehi, dia_semana, destino))
                 print '%s,%s,%s' % (tipo_vehi, dia_semana, destino)
