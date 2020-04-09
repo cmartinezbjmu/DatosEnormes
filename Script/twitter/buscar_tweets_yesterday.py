cbfb719e94b14b3bd13a77e03511040aedde7ee3
jcardenascAndes93 <j.cardenasc@uniandes.edu.co>
2020-04-09 13:01:59 -0500

    Incuye script para filtrar los tweets del dia anterior

--- Script/twitter/buscar_tweets_yesterday.py
+++ Script/twitter/buscar_tweets_yesterday.py
@@ -6,27 +6,23 @@ from pymongo import MongoClient, errors
 
 palabras_clave = ["Coronavirus", "covid19"]
 
-consumer_key = "FGqkOJU3o3Rh15alV5Ql4obUX"
-consumer_secret = "2vIiKctsxxezL6kKGo4qtyFPDcY1viFz6M1a2bHLTwWrijVXE5"
-access_token = "151179935-HLh8FmhbS0D3892npnkfiM6UNc1hLZ0NRHKhZCzJ"
-access_token_secret = "tiKTQPpveXHZRsEm0ODUeCJGmlSBqqeUzO7F7cRYKrTp6"
+#consumer_key = "FGqkOJU3o3Rh15alV5Ql4obUX"
+#consumer_secret = "2vIiKctsxxezL6kKGo4qtyFPDcY1viFz6M1a2bHLTwWrijVXE5"
+#access_token = "151179935-HLh8FmhbS0D3892npnkfiM6UNc1hLZ0NRHKhZCzJ"
+#access_token_secret = "tiKTQPpveXHZRsEm0ODUeCJGmlSBqqeUzO7F7cRYKrTp6"
 
-# consumer_key = "hwQLsT9LMp90MPSgYXf7Abvzw"
-# consumer_secret = "wSYKwJPPvaAvPD2KL8dGrkm8kJORCIPss5qVLMuLMAZxWGUcb8"
-# access_token = "52602353-a7v7fPQ90BvjVNBR1ZYg6ziiHCiyqfmzdcuS150em"
-# access_token_secret = "roy6xRVWbYF2K2BNog9Fi04lArpuNLmY8OIMJWkChrEmW"
+consumer_key = "hwQLsT9LMp90MPSgYXf7Abvzw"
+consumer_secret = "wSYKwJPPvaAvPD2KL8dGrkm8kJORCIPss5qVLMuLMAZxWGUcb8"
+access_token = "52602353-a7v7fPQ90BvjVNBR1ZYg6ziiHCiyqfmzdcuS150em"
+access_token_secret = "roy6xRVWbYF2K2BNog9Fi04lArpuNLmY8OIMJWkChrEmW"
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-cuentas2 = {
-    "claudia_lopez": {"id":"137908875",
-                      "screen_name": "claudialopez"},
-}
-
-
-cuentas = ['ivanduque', 'jorgeivanospina', 'quinterocalle', 'minsaludcol', 'revistasemana', 'noticiascaracol', 'noticiasrcn', 'bluradioco', 'lafm', 'mabellaranews']
+cuentas = ['ivanduque', 'jorgeivanospina', 'quinterocalle', 'minsaludcol',
+           'revistasemana', 'noticiascaracol', 'noticiasrcn', 'bluradioco',
+           'lafm', 'mabellaranews']
 
 def retrieve_replied_tweet(id_tweet):
     resultado = None
@@ -59,7 +55,7 @@ def retrieve_replied_tweet(id_tweet):
 
 def retrieve_initial_tweets(screen_name):
     replys = []
-    for tweet in tweepy.Cursor(api.search, q='{}'.format(screen_name), lang="es", tweet_mode='extended').items(1000000):
+    for tweet in tweepy.Cursor(api.search, q='{}'.format(screen_name), lang="es", tweet_mode='extended').items(10):
         if (tweet._json['in_reply_to_status_id']) or (tweet._json['is_quote_status'] == True):
             replys.append(tweet)
         #print(json.dumps(tweet._json, ensure_ascii=False).encode('utf8').decode())
@@ -155,7 +151,7 @@ palabra_clave = [
 
 # Busco los últimos 100 tweets
 for i in cuentas:
-    tweets = tweepy.Cursor(api.user_timeline, screen_name=i, tweet_mode='extended').items(500)
+    tweets = tweepy.Cursor(api.user_timeline, screen_name=i, tweet_mode='extended').items(10)
     initial_tweets = retrieve_initial_tweets(i)
     resultado = dict()
     json_result = dict()
@@ -169,28 +165,31 @@ for i in cuentas:
         collection = db.COL_tweets
 
         for tweet in tweets:    
-            if any(palabra in tweet.full_text.lower() for palabra in palabra_clave):
+            #if any(palabra in tweet.full_text.lower() for palabra in palabra_clave): Tue Apr 07 13:09:26 +0000 2020
+            tweet_date = datetime.datetime.strptime(tweet._json['created_at'], '%a %b %d %H:%M:%S %z %Y')
+            yesterday = datetime.date.today() - datetime.timedelta(1)
+            # Filtro por tweets del dia anterior
+            if tweet_date.date() == yesterday:
                 resultado = json.dumps({'created_at': tweet._json['created_at'],
-                                    'id': tweet._json['id'],
-                                    'full_text': tweet._json['full_text'],
-                                    'hashtags': tweet._json['entities']['hashtags'],
-                                    'user_mentions': tweet._json['entities']['user_mentions'],
-                                    'urls': tweet._json['entities']['urls'],
-                                    'user': { 'id': tweet._json['user']['id'],
-                                            'name': tweet._json['user']['name'],
-                                            'screen_name': tweet._json['user']['screen_name'],
-                                            'followers_count': tweet._json['user']['followers_count']},
-                                    'retweet_count': tweet._json['retweet_count'],
-                                    'favorite_count': tweet._json['favorite_count'],
-                                    'in_reply_to_status_id': tweet._json['in_reply_to_status_id'],
-                                    'is_quote_status': tweet._json['is_quote_status'],
-                                }, indent=4, ensure_ascii=False)
-                
+                                'id': tweet._json['id'],
+                                'full_text': tweet._json['full_text'],
+                                'hashtags': tweet._json['entities']['hashtags'],
+                                'user_mentions': tweet._json['entities']['user_mentions'],
+                                'urls': tweet._json['entities']['urls'],
+                                'user': { 'id': tweet._json['user']['id'],
+                                        'name': tweet._json['user']['name'],
+                                        'screen_name': tweet._json['user']['screen_name'],
+                                        'followers_count': tweet._json['user']['followers_count']},
+                                'retweet_count': tweet._json['retweet_count'],
+                                'favorite_count': tweet._json['favorite_count'],
+                                'in_reply_to_status_id': tweet._json['in_reply_to_status_id'],
+                                'is_quote_status': tweet._json['is_quote_status'],
+                            }, indent=4, ensure_ascii=False)
+            
                 json_result = (json.loads(resultado))
                 # Valida si es un reply
                 if tweet._json['in_reply_to_status_id'] != None:
                     json_result['status_replied'] = retrieve_replied_tweet(tweet._json['in_reply_to_status_id'])
-
                 # Valida si es una cita
                 if tweet._json['is_quote_status'] == True:
                     try:
@@ -209,10 +208,8 @@ for i in cuentas:
                                                                 }, indent=4, ensure_ascii=False))
                     except KeyError as e:
                         continue
-
                 json_result['replys'] = retrieve_all_replys(tweet._json['id'], tweet._json['user']['id'], initial_tweets)
                 json_result['quotes'] = retrieve_all_quotes(tweet._json['id'], tweet._json['user']['id'], initial_tweets)
-
                 #print(json.dumps(json_result, ensure_ascii=False).encode('utf8').decode())
                 post_id = collection.insert_one(json_result).inserted_id
 
