11749e4a2be15a806d761e6b492c22e4c03b32f9
d-ocampo <daocampol@unal.edu.co>
2020-04-25 12:40:31 -0500

    Panel de control de modelo de sentimientos

--- Script/twitter/cotweet/navbar.py
+++ Script/twitter/cotweet/navbar.py
@@ -3,7 +3,8 @@ def Navbar():
    navbar = dbc.NavbarSimple(className="encabezado",
       children=[
          dbc.NavItem(dbc.NavLink("Entrenamiento", href="/apps/model")),
-	 dbc.NavItem(dbc.NavLink("Temas", href="/apps/temas"))
+	      dbc.NavItem(dbc.NavLink("Temas", href="/apps/temas")),
+         dbc.NavItem(dbc.NavLink("Modelos", href="/apps/prediccion"))
       ],
       brand="Home",
       brand_href="/",
