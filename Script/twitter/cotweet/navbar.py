0941db6e47d2762c17b1c343cc54c8d0c8a0feea
jcardenascAndes93 <j.cardenasc@uniandes.edu.co>
2020-04-26 22:51:10 -0500

    Agrega la vista de analisis por medio de tabs

--- Script/twitter/cotweet/navbar.py
+++ Script/twitter/cotweet/navbar.py
@@ -3,7 +3,7 @@ def Navbar():
    navbar = dbc.NavbarSimple(className="encabezado",
       children=[
          dbc.NavItem(dbc.NavLink("Entrenamiento", href="/apps/model")),
-	      dbc.NavItem(dbc.NavLink("Tendencia", href="/apps/tendencia")),
+         dbc.NavItem(dbc.NavLink("Analisis Influencers", href="/apps/analisis")),
          dbc.NavItem(dbc.NavLink("Modelos", href="/apps/prediccion")),
          dbc.NavItem(dbc.NavLink("Top temas", href="/apps/top_temas"))
       ],
