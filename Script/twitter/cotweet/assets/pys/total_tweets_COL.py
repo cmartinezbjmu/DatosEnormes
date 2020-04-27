201a32dd88b20177868b90ebb59b6d085cc7f934
jcardenascAndes93 <j.cardenasc@uniandes.edu.co>
2020-04-27 06:01:42 -0500

    Correccion totales colombia

--- Script/twitter/cotweet/assets/pys/total_tweets_COL.py
+++ Script/twitter/cotweet/assets/pys/total_tweets_COL.py
@@ -12,32 +12,32 @@ reduce = Code("function (key, values) {"
 
 
 def cuenta_tweets():
-    map = Code("function () {"
+    mapper = Code("function () {"
             "var tweets = this.id;"                       
             "emit(tweets, 1);"
             "}")
-    return map
+    return mapper
 
 
 def cuenta_replys():
-    map = Code("function () {"            
+    mapper = Code("function () {"            
             "var tweet_replys = this.replys;"
             "tweet_replys.forEach(function(y) {"
             "emit(y['id'], 1);"
             "});" 
           "}")
     
-    return map
+    return mapper
 
 def cuenta_quotes():
-    map = Code("function () {"            
+    mapper = Code("function () {"            
                 "var tweet_quotes = this.quotes;"
                 "tweet_quotes.forEach(function(y) {"
                 "emit(y['id'], 1);"
                 "});" 
             "}")
     
-    return map
+    return mapper
 
 
 def cuenta_total():
